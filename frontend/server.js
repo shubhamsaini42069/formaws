e7c95d38560409720916104b39c8a272ffb71e02
Cloud User <ec2-user@ip-172-31-11-20.ap-south-1.compute.internal>
2025-07-14 08:33:11 +0000

    this is aws updated form

--- frontend/server.js
+++ frontend/server.js
@@ -2,5 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files from 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3000, () => console.log('Frontend running on http://localhost:3000'));
+
+// Listen on all interfaces so it's accessible from EC2 public IP
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Frontend running on http://<your-ec2-public-ip>:3000');
+});
+
