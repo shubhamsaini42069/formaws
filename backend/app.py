c9e08617b183ed52ae24d61af31d48bc0d11c120
Shubham Saini <ssaini@localhost.localdomain>
2025-07-24 21:28:17 +0530

    more updates

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
+
 submitted_data = []
 
 @app.route('/submit', methods=['POST'])
@@ -11,33 +13,33 @@ def submit():
     name = data.get('name')
     email = data.get('email')
     
-    # Save to the list
     submitted_data.append({'name': name, 'email': email})
-    
-    return jsonify({'message': f'got your name {name} ({email})'})
+    return jsonify({'message': f'Got your name {name} ({email})'})
 
 @app.route('/view', methods=['GET'])
 def view():
     return jsonify(submitted_data)
+
 @app.route('/')
 def index():
     return "AWS Flask App is running!"
+
 @app.route('/get-ip')
 def get_ip():
-    # Secure fetch of public IPv4 with token
-    token = requests.put(
-        "http://169.254.169.254/latest/api/token",
-        headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"}
-    ).text
+    try:
+        token = requests.put(
+            "http://169.254.169.254/latest/api/token",
+            headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"}
+        ).text
 
-    ip = requests.get(
-        "http://169.254.169.254/latest/meta-data/public-ipv4",
-        headers={"X-aws-ec2-metadata-token": token}
-    ).text
+        ip = requests.get(
+            "http://169.254.169.254/latest/meta-data/public-ipv4",
+            headers={"X-aws-ec2-metadata-token": token}
+        ).text
 
-    return jsonify({"ip": ip})
-
-
+        return jsonify({"ip": ip})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
