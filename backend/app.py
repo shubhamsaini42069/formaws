c4d0e934f3e92ceac146246bf00f1e2fd981d507
Shubham Saini <neenusaini86@gmail.com>
2025-07-31 16:08:45 +0530

    Update app.py
    changed

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def view():
     return jsonify(submitted_data)
 @app.route('/')
 def index():
-    return "AWS Flask App is running!"
+    return "jenkins changed "
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
